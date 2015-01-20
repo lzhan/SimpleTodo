9f2d196c372070dd212f29a2b3d5448651cb9181
Lichun Zhan <lzhan@highduskthey-lm.corp.yahoo.com>
2015-01-20 09:52:55 -0800

    add app name back

--- app/src/main/java/com/codepath/simpletodo/MainActivity.java
+++ app/src/main/java/com/codepath/simpletodo/MainActivity.java
@@ -3,6 +3,7 @@ package com.codepath.simpletodo;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
@@ -17,7 +18,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
     ArrayList<String> items;
     ArrayAdapter<String> itemsAdapter;
     ListView lvItems;
